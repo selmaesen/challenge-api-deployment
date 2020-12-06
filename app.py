690f37dbeda4b8805337adc739d4faba4305f238
Davy Nimbona <davynimbona@gmail.com>
2020-12-07 00:58:03 +0100

    Fixed way getting json to dataframe

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 from flask import Flask, request, jsonify, render_template
 import pickle
-from pipeline.predict import prediction
+from pipeline.predict.prediction import predict
+from pipeline.preprocessing.cleaning_data import preprocess
 import pipeline.model as model
 
 app = Flask(__name__)
@@ -11,16 +12,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     """
-    function returns string on home page
-    parameters: GET
-    return: "Welcome to API Deployment"
-    """
-    return "Welcome to API Deployment"
-
-
-@app.route('/welcome')
-def welcome():
-    """
     function returns string on welcome page
     parameters: GET
     return: "Alive!"
@@ -28,20 +19,31 @@ def welcome():
     return "Alive!"
 
 
-@app.route("/predict", methods=["POST", "GET"])
-def predict():
+@app.route('/welcome')
+def welcome():
+    """
+    function returns string on home page
+    parameters: GET
+    return: "Welcome to API Deployment"
+    """
+    return "Welcome to API Deployment"
+
+
+@app.route("/predict", methods=["POST"])
+def predict_api():
     """
     function returns predicted price
     parameters: GET,POST
     return: "The predicted price is VALUE"
     """
     data = request.get_json()
-    dataset = pd.DataFrame([data])
-    result = prediction.predict(dataset)
+    dataset = pd.DataFrame(data, index=[0,])
+    new_df = preprocess(dataset)
+    new_df.to_csv("test.csv")
+    result = predict(new_df)
 
-    return jsonify(("The predicted price is {}".format(result)))
+    return jsonify(result)
 
 
 if __name__ == '__main__':
-    # run app on port 5000
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
