103544b61222fe3c4801ab0100bdcfa6128c4f7b
Davy Nimbona <davynimbona@gmail.com>
2020-12-08 12:01:42 +0100

    New house data preprocessing functions

---
+++ pipeline/preprocessing/clean_test.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import numpy as np
+import missingno as msno
+import re
+
+def define_province(new_df, code):
+    if ((code >= 1000) & (code < 1299)):
+        new_df["province_Brussels Capital Region"] = 1
+    elif ((code >= 1300) & (code < 1499)):
+        new_df["province_Walloon Brabant"] = 1
+    elif (((code >= 1500) & (code < 1999)) | ((code >= 3000) & (code < 3499))):
+        new_df["province_Flemish Brabant"] = 1
+    elif ((code >= 2000) & (code < 2999)):
+        new_df["province_Antwerp"] = 1
+    elif ((code >= 3500) & (code < 3999)):
+        new_df["province_Limburg"] = 1
+    elif ((code >= 4000) & (code < 4999)):
+        new_df["province_Liège"] = 1
+    elif ((code >= 5000) & (code < 5999)):
+        new_df["province_Namur"] = 1
+    elif (((code >= 6000) & (code < 6599)) | ((code >= 7000) & (code < 7999))):
+        new_df["province_Hainaut"] = 1
+    elif ((code >= 6600) & (code < 6999)):
+        new_df["province_Luxembourg"] = 1
+    elif ((code >= 8000) & (code < 8999)):
+        new_df["province_West Flanders"] = 1
+    elif ((code >= 9000) & (code < 9999)):
+        new_df["province_East Flanders"] = 1
+
+    return new_df
+
+
+def define_property(new_df, type):
+    if type == "HOUSE":
+        new_df["property-type_HOUSE"] = 1
+    elif type == "APARTMENT":
+        new_df["property-type_APARTMENT"] = 1
+    elif type == "OTHERS":
+        new_df["property-type_OTHERS"] = 1
+    
+    return new_df
+
+
+def preprocess(df):
+    mandatory = ["area", "property-type", "rooms-number", "zip-code"]
+
+    for m in mandatory:
+        if m not in df.columns:
+            return "Error-Missing!"
+
+    if df["property-type"].values[0] not in ["APARTMENT", "HOUSE", "OTHERS"]:
+        return "Error-Property"
+    
+    if df["zip-code"].values[0] < 1000 or df["zip-code"].values[0] >= 9999:
+        return "Error-ZipCode"
+    
+    new_df = pd.read_csv("pipeline/preprocessing/test-dataframe.csv")
+    new_df.drop(["Unnamed: 0"], axis=1, inplace=True)
+    new_df = define_province(new_df, df["zip-code"].values[0])
+    new_df = define_property(new_df, df["property-type"].values[0])
+
+    columns = [column for column in df.columns if column not in ["property-type", "zip-code"]]
+    new_df[columns] = df[columns]
+    new_df.to_csv("Test.csv")
+    print("\n\n\n")
+    # print(new_df["province_Brussels Capital Region"])
+    print("\n\n\n")
+
+
+# if __name__ == "__main__":
+#     data = pd.read_csv("ready_to_model_df.csv")
+#     data.drop(["Unnamed: 0"], axis=1, inplace=True)
+#     print(data.columns)
+#     print(preprocess(data))
