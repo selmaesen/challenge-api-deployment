bb2d752efa76877cb5d7c49ddc32c4556ac4a90a
Davy Nimbona <davynimbona@gmail.com>
2020-12-08 11:58:41 +0100

    Handle error message

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import pandas as pd
 from flask import Flask, request, jsonify, render_template
 import pickle
 from pipeline.predict.prediction import predict
-from pipeline.preprocessing.cleaning_data_vers02 import preprocess
+# from pipeline.preprocessing.cleaning_data_vers02 import preprocess
+from pipeline.preprocessing.clean_test import preprocess
 import pipeline.model as model
 
 app = Flask(__name__)
@@ -38,17 +39,23 @@ def predict_api():
     """
     data = request.get_json()
     dataset = pd.DataFrame(data, index=[0, ])
+    dataset.replace(True, int(1), inplace=True)
+    dataset.replace(False, int(0), inplace=True)
+    # dataset.replace(1.0, int(1), inplace=True)
     new_df = preprocess(dataset)
 
     print(type(new_df))
     if isinstance(new_df, str):
-
-        return "Error!"
+        if new_df == "Error-Missing!":
+            return "Area, property-type, rooms-number or zip-code is missing"
+        elif new_df == "Error-Property":
+            return "Only input APARTMENT, HOUSE or OTHERS for property"
+        elif new_df == "Error-ZipCode":
+            return "Wrong zip-code input. Must be [1000-9998]"
     else:
-
         result = predict(new_df)
         return jsonify(result)
 
 
 if __name__ == '__main__':
-    app.run(post=5000,host='0.0.0.0')
+    app.run(debug=True)
